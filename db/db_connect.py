3e1dd0c68d5218548625e18a376db51a9d288774
Praveenprabhas <devichinthalapudi2001@gmail.com>
2025-03-20 09:29:13 +0530

    note

--- db/db_connect.py
+++ db/db_connect.py
@@ -6,10 +6,4 @@ client = MongoClient('localhost',27017)
 db_name = client['password_manager']
 
 
-appdb_collection = db_name['appdata']
-webdb_collection = db_name['webdata']
-logindb_collection = db_name['login']
 
-
-# print(client.list_database_names())
-# print(db_name['database'])
